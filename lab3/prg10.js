d685947db65f78ab8922a389c24cd28b13be85bc
Balbhadra Singh <balbhadra.25b01010361@abes.ac.in>
2026-09-08 09:02:05 +0530

    add route

---
+++ lab3/prg10.js
@@ -0,0 +1,21 @@
+import http from "http";
+import { createReadStream } from "fs";
+const server = http.createServer((req, res) => {
+  console.log("method:", req.method);
+
+  if (req.url === "/" && req.method === "GET") {
+    res.end("<h1>Products details</h1>");
+  } else if (req.url === "/products" && req.method === "GET") {
+    res.writeHead(200, { "content-type": "text/json" });
+
+    const stream = createReadStream("./data/products.json", {
+      encoding: "utf-8",
+    });
+    stream.pipe(res);
+  } else {
+    res.statusCode = 404;
+    res.end("not found");
+  }
+});
+
+server.listen(3000, () => console.log("prg10 is running at port 3000"));
