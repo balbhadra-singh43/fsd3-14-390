fea4d57a5d1ae4fbd1b742ad1d8ceeb405b56788
Balbhadra Singh <balbhadra.25b01010361@abes.ac.in>
2026-09-17 14:35:16 +0530

    sending html file to the client

---
+++ lab5/prg1.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import path from 'path'
+import { fileURLToPath } from "node:url";
+
+
+const port = 3333;
+const app = express();
+
+const filename = fileURLToPath(import.meta.url);
+const dirname= path.dirname(filename);
+
+app.get("/", (req,res) => {
+    res.sendFile(path.join(dirname, "public", "index.html"));
+});
+
+app.get("/", (req,res) => {
+    res.sendFile(path.join(dirname, "public", "index.html"));
+});
+
+app.listen(port,()=> console.log("prg1 is running at ", port));
